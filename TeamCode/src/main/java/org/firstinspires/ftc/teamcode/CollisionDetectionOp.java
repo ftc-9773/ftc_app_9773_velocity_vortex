fc551ace441a994c6f290025d8c753394677549b
michaela10c <michaela10c@qq.com>
2016-09-30 19:15:12 -0400

    Almost there................

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CollisionDetectionOp.java
@@ -0,0 +1,20 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.kauailabs.navx.ftc.AHRS;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+/**
+ * Created by michaelzhou on 9/30/16.
+ */
+
+public class CollisionDetectionOp extends LinearOpMode {
+
+    private final int NAVX_DIM_I2C_PORT = 0;
+
+    private final double collisionThreshold = 0.5;
+
+
+    public void runOpMode() throws InterruptedException{
+
+    }
+}
