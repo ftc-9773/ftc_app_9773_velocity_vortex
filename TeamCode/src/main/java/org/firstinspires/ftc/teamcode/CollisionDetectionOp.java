2984bef26a8937d32736a66a8763035bb40aeee2
michaela10c <michaela10c@qq.com>
2016-10-03 16:16:49 -0400

    Collision Detection program complete - for testing

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CollisionDetectionOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CollisionDetectionOp.java
@@ -1,20 +1,69 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.kauailabs.navx.ftc.AHRS;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.util.ElapsedTime;
 
 /**
  * Created by michaelzhou on 9/30/16.
  */
 
-//public class CollisionDetectionOp extends LinearOpMode {
-//
-//    private final int NAVX_DIM_I2C_PORT = 0;
-//
-//    private final double collisionThreshold = 0.5;
-//
-//
-//    public void runOpMode() throws InterruptedException{
-//
-//    }
-//}
+@Autonomous(name = "Collision Detection", group = "Autonomous")
+public class CollisionDetectionOp extends LinearOpMode {
+
+    private final int NAVX_DIM_I2C_PORT = 0;
+    private final double collisionThreshold = 0.5;
+    double lastAccelX,lastAccelY;
+    private AHRS navx_device;
+    private boolean collisionState;
+    private ElapsedTime runtime = new ElapsedTime();
+
+    private long last_system_timestamp = 0;
+    private long last_sensor_timestamp = 0;
+
+    private long sensor_timestamp_delta = 0;
+    private long system_timestamp_delta = 0;
+
+    public void runOpMode() throws InterruptedException{
+        //init
+        navx_device = AHRS.getInstance(hardwareMap.deviceInterfaceModule.get("dim"), NAVX_DIM_I2C_PORT,
+                AHRS.DeviceDataType.kProcessedData);
+        lastAccelX = 0.0; lastAccelY = 0.0; setCollision(false);
+
+        waitForStart();
+        telemetry.addData("navX Op Init Loop", runtime.toString());
+        while (opModeIsActive()){
+            telemetry.addData("1. NavX device", navx_device.isConnected() ? "Connected" : "Not connected");
+            String gyroCal, motion;
+            if(navx_device.isConnected()){
+                gyroCal = (navx_device.isCalibrating() ? "Calibrating" : "Calibration completed");
+                motion = (navx_device.isMoving() ? "Moving" : "Not Moving");
+                if(navx_device.isRotating()){
+                    motion += ", Rotating";
+                }
+            }
+            else{
+                gyroCal = motion = "------";
+            }
+            telemetry.addData("2. GyroAccel", gyroCal);
+            telemetry.addData("3. Motion", motion);
+            telemetry.addData("4. Collision", getCollision());
+            telemetry.addData("5. Timing", Long.toString(sensor_timestamp_delta) + ", " +
+                    Long.toString(system_timestamp_delta));
+            telemetry.addData("6 Events", Double.toString(navx_device.getUpdateCount()));
+        }
+
+        idle();
+    }
+
+    //collision state getter
+    private String getCollision(){
+        return collisionState ? "COLLISION" : "NO COLLISION";
+    }
+
+    //collision state setter
+    private void setCollision(boolean newValue){
+        this.collisionState = newValue;
+    }
+}
