7f4140d38dfc81f633b01e5fa49818326e930d90
Pranav Burugula <pranav.burugula@gmail.com>
2016-10-03 17:35:22 -0400

    fixed bug

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomousRecord.java
@@ -0,0 +1,21 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+import org.firstinspires.ftc.teamcode.drivesys.TwoMotorDrive;
+
+/**
+ * Created by pb8xe_000 on 10/3/2016.
+ */
+
+public class AutonomousRecord extends LinearOpMode {
+    DcMotor motorL;
+    DcMotor motorR;
+    TwoMotorDrive drivesys;
+
+    @Override
+    public void runOpMode(){
+
+    }
+}
