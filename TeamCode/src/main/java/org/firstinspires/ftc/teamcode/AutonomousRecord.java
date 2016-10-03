dccae3a98ae15a1bfaacd33725ff7e45baa05dda
Pranav Burugula <pranav.burugula@gmail.com>
2016-10-03 17:58:02 -0400

    finished 2wd record replay

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomousRecord.java
@@ -0,0 +1,37 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+import org.firstinspires.ftc.teamcode.drivesys.TwoMotorDrive;
+import org.firstinspires.ftc.teamcode.drivesys.Wheel;
+import org.firstinspires.ftc.teamcode.util.FileRW;
+
+@Autonomous(name = "Record", group = "Record")
+public class AutonomousRecord extends LinearOpMode {
+    DcMotor motorL;
+    DcMotor motorR;
+    TwoMotorDrive drivesys;
+    Wheel wheel;
+    FileRW fileRW;
+
+    @Override
+    public void runOpMode() throws InterruptedException{
+        motorL = hardwareMap.dcMotor.get("motorL");
+        motorR = hardwareMap.dcMotor.get("motorR");
+        wheel = new Wheel(Wheel.Type.RUBBER_TREADED);
+        drivesys = new TwoMotorDrive(motorL, motorR, 1, 0, 1, 1, wheel);
+        fileRW = new FileRW("/sdcard/FIRST/autonomous/autonomousPath.txt", true);
+
+        waitForStart();
+        while (opModeIsActive()){
+            double speed = gamepad1.left_stick_y;
+            double direction = gamepad1.right_stick_x;
+
+            fileRW.fileWrite(Double.toString(speed) + "," + Double.toString(direction));
+
+            idle();
+        }
+    }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomousReplay.java
@@ -0,0 +1,39 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+import org.firstinspires.ftc.teamcode.drivesys.TwoMotorDrive;
+import org.firstinspires.ftc.teamcode.drivesys.Wheel;
+import org.firstinspires.ftc.teamcode.util.FileRW;
+
+@Autonomous(name = "Replay", group = "Autonomous")
+public class AutonomousReplay extends LinearOpMode {
+    DcMotor motorL;
+    DcMotor motorR;
+    TwoMotorDrive drivesys;
+    Wheel wheel;
+    FileRW fileRW;
+
+    @Override
+    public void runOpMode() throws InterruptedException{
+        motorL = hardwareMap.dcMotor.get("motorL");
+        motorR = hardwareMap.dcMotor.get("motorR");
+        wheel = new Wheel(Wheel.Type.RUBBER_TREADED);
+        drivesys = new TwoMotorDrive(motorL, motorR, 1, 0, 1, 1, wheel);
+        fileRW = new FileRW("/sdcard/FIRST/autonomous/autonomousPath.txt", false);
+
+        waitForStart();
+        while (opModeIsActive()){
+            String line = fileRW.getNextLine();
+            String[] lineElements = line.split(",");
+
+            double speed = Double.parseDouble(lineElements[0]);
+            double direction = Double.parseDouble(lineElements[1]);
+
+            drivesys.drive((float)speed, (float)direction);
+            idle();
+        }
+    }
+}
