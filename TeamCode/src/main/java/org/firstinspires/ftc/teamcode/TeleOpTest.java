08decc95b774f755eac41a3fa5ea6932748dd557
Pranav Burugula <pranav.burugula@gmail.com>
2016-10-03 19:32:17 -0400

    6wd

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpTest.java
@@ -8,23 +8,29 @@ import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
+
+import org.firstinspires.ftc.teamcode.drivesys.FourMotorTankDrive;
 import org.firstinspires.ftc.teamcode.drivesys.Wheel;
 
 import org.firstinspires.ftc.teamcode.drivesys.TwoMotorDrive;
 
 @TeleOp(name = "TeleOpTest", group = "TeleOp")
 public class TeleOpTest extends LinearOpMode{
-    DcMotor motorL;
-    DcMotor motorR;
-    TwoMotorDrive drivesys;
+    DcMotor motorL1;
+    DcMotor motorL2;
+    DcMotor motorR1;
+    DcMotor motorR2;
+    FourMotorTankDrive drivesys;
     Wheel wheel;
 
     @Override
     public void runOpMode() throws InterruptedException{
-        motorL = hardwareMap.dcMotor.get("motorL");
-        motorR = hardwareMap.dcMotor.get("motorR");
+        motorL1 = hardwareMap.dcMotor.get("motorL1");
+        motorL2 = hardwareMap.dcMotor.get("motorL2");
+        motorR1 = hardwareMap.dcMotor.get("motorR1");
+        motorR2 = hardwareMap.dcMotor.get("motorR2");
         wheel = new Wheel(Wheel.Type.RUBBER_TREADED);
-        drivesys = new TwoMotorDrive(motorL, motorR, 1, 0, 1, 1, wheel);
+        drivesys = new FourMotorTankDrive(motorL1, motorL2, motorR1, motorR2, 1, 0, 1, 1, wheel);
 
         waitForStart();
         while (opModeIsActive()){
