a329ee9822c093377c2791e03541ba570592ed41
michaela10c <michaela10c@qq.com>
2016-10-16 18:15:53 -0400

    Copied Replay from Luke's branch

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomousReplay.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomousReplay.java
@@ -1,28 +1,65 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.ftccommon.DbgLog;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 
-import org.firstinspires.ftc.teamcode.drivesys.TwoMotorDrive;
+import org.firstinspires.ftc.teamcode.drivesys.FourMotorTankDrive;
 import org.firstinspires.ftc.teamcode.drivesys.Wheel;
+import org.firstinspires.ftc.teamcode.util.AutonomousConfigReader;
+import org.firstinspires.ftc.teamcode.util.DriveSysReader;
 import org.firstinspires.ftc.teamcode.util.FileRW;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 @Autonomous(name = "Replay", group = "Autonomous")
 public class AutonomousReplay extends LinearOpMode {
     DcMotor motorL;
     DcMotor motorR;
-    TwoMotorDrive drivesys;
     Wheel wheel;
     FileRW fileRW;
+    FourMotorTankDrive fourMotorTankDrive;
 
     @Override
     public void runOpMode() throws InterruptedException{
-        motorL = hardwareMap.dcMotor.get("motorL");
-        motorR = hardwareMap.dcMotor.get("motorR");
-        wheel = new Wheel(Wheel.Type.RUBBER_TREADED, 4.0);
-        drivesys = new TwoMotorDrive(motorL, motorR, 1, 0, 1, wheel, 1120);
-        fileRW = new FileRW("/sdcard/FIRST/autonomous/autonomousPath.txt", false);
+        AutonomousConfigReader autonomousConfigReader = new AutonomousConfigReader("/sdcard/FIRST/autonomous_config.json");
+        DriveSysReader driveSysReader = autonomousConfigReader.getDriveSysReader();
+        try{
+            if(driveSysReader.getDriveSysType() == "4WD"){
+                int numMotors = 4;
+                JSONObject motors = driveSysReader.getMotors();
+                JSONObject fMotorLObj = motors.getJSONObject("fMotorL");
+                JSONObject fMotorRObj = motors.getJSONObject("fMotorR");
+                JSONObject rMotorLObj = motors.getJSONObject("rMotorL");
+                JSONObject rMotorRObj = motors.getJSONObject("rMotorR");
+
+                DcMotor fMotorL = hardwareMap.dcMotor.get(fMotorLObj.getString("name"));
+                DcMotor fMotorR = hardwareMap.dcMotor.get(fMotorRObj.getString("name"));
+                DcMotor rMotorL = hardwareMap.dcMotor.get(rMotorLObj.getString("name"));
+                DcMotor rMotorR = hardwareMap.dcMotor.get(rMotorRObj.getString("name"));
+
+                if(driveSysReader.getWheelType() == "rubber-treaded"){
+                    wheel = new Wheel(Wheel.Type.RUBBER_TREADED, driveSysReader.getWheelDiameter());
+                }
+
+
+                fourMotorTankDrive = new FourMotorTankDrive(fMotorL,rMotorL, fMotorR, rMotorR,
+                        1, 0, 1.0, wheel, fMotorLObj.getInt("motorCPR"));
+            }
+        }catch (JSONException e){
+            e.printStackTrace();
+        }
+
+        // ToDo:  Get the record-replay file path from the json config file.
+        String fileName = null;
+        try{
+            fileName = autonomousConfigReader.jsonRoot.getString("replayFileName");
+        }
+        catch(JSONException exception){
+            DbgLog.error("Darn it, JSON flipped out again.");
+        }
+        fileRW = new FileRW(fileName, false);
 
         waitForStart();
         while (opModeIsActive()){
@@ -38,7 +75,7 @@ public class AutonomousReplay extends LinearOpMode {
                     double direction = Double.parseDouble(lineElements[1]);
 
                     //sleep(timestamp);
-                    drivesys.drive((float) speed, (float) direction);
+                    fourMotorTankDrive.drive((float) speed, (float) direction);
                 }
             }
             else{
