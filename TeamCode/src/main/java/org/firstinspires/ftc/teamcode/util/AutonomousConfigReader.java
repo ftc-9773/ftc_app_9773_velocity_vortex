d1dccddf96e88c142f044c2fc5f2e9535e6d4760
michaela10c <michaela10c@qq.com>
2016-10-16 18:16:59 -0400

    Recopied Luke's ConfigReader code to master

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/AutonomousConfigReader.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/AutonomousConfigReader.java
@@ -1,5 +1,7 @@
 package org.firstinspires.ftc.teamcode.util;
 
+import org.json.JSONException;
+
 /**
  * Created by ftcrobocracy on 10/14/16.
  */
@@ -7,5 +9,18 @@ package org.firstinspires.ftc.teamcode.util;
 public class AutonomousConfigReader extends JsonReader {
     public AutonomousConfigReader(String filePath) {
         super(filePath);
+    }
+
+    public DriveSysReader getDriveSysReader () {
+        DriveSysReader driveSysReader = null;
+        String driveSysFilePath;
+        try {
+            driveSysFilePath = this.jsonRoot.getString("robotConfigFilePath");
+            driveSysReader = new DriveSysReader(driveSysFilePath);
+        }
+        catch (JSONException exc) {
+            exc.printStackTrace();
+        }
+        return (driveSysReader);
     }
 }
