5c1a09b59cdc9a6f4688123658855fad73171695
BuildTools <unconfigured@null.spigotmc.org>
2016-11-27 09:00:32 -0500

    modifying

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/recordreplay/RecordReplaySys.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/recordreplay/RecordReplaySys.java
@@ -3,6 +3,7 @@ package org.firstinspires.ftc.teamcode.recordreplay;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
 import org.firstinspires.ftc.teamcode.FTCRobot;
+import org.firstinspires.ftc.teamcode.util.FileRW;
 import org.firstinspires.ftc.teamcode.util.JsonReaders.JsonReader;
 import org.firstinspires.ftc.teamcode.util.JsonReaders.RecordReplaySysReader;
 
@@ -15,22 +16,31 @@ public abstract class RecordReplaySys {
     FTCRobot robot;
     String recordType;
     String filePath;
+    FileRW fileRW;
 
 
-    public RecordReplaySys(LinearOpMode curOpMode, FTCRobot robot, String recordType, String filePath){
+    public RecordReplaySys(LinearOpMode curOpMode, FTCRobot robot, String recordType, String filePath, FileRW fileRW){
         this.curOpMode = curOpMode;
         this.robot = robot;
         this.filePath = filePath;
         this.recordType = recordType;
+        this.fileRW = fileRW;
     }
+
+    public RecordReplaySys(){}
 
     public static RecordReplaySys createRecordReplaySystem(LinearOpMode curOpMode, FTCRobot robot, String recordType,
                                                            String filePath){
         RecordReplaySys recordReplaySys = null;
         RecordReplaySysReader recordReplaySysReader = new RecordReplaySysReader(JsonReader.recordReplaySysFile, recordType);
 
-        switch (recordType){
 
+        switch (recordType){
+            case "sensor":
+                String[] columns = recordReplaySysReader.getColumns();
+                SensorRecord sensorRecord = new SensorRecord(curOpMode, robot, filePath, columns);
+
+                sensorRecord.
         }
 
         return recordReplaySys;
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/recordreplay/SensorRecord.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/recordreplay/SensorRecord.java
@@ -1,8 +1,25 @@
 package org.firstinspires.ftc.teamcode.recordreplay;
 
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.firstinspires.ftc.teamcode.FTCRobot;
+import org.firstinspires.ftc.teamcode.util.FileRW;
+
 /**
  * Created by rsburugula on 11/26/16.
  */
 
 public class SensorRecord extends RecordReplaySys {
+    String filePath;
+    String[] columns = null;
+    FileRW fileRW;
+    LinearOpMode curOpMode;
+    FTCRobot robot;
+
+    public SensorRecord(LinearOpMode curOpMode, FTCRobot robot, String filePath, String[] columns){
+        this.curOpMode = curOpMode;
+        this.robot = robot;
+        this.filePath = filePath;
+        this.fileRW = new FileRW(filePath, true);
+    }
 }
