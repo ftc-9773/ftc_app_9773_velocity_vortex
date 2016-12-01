2c787dfa37b31e8a3273bc8ca369e5824f1b1da6
ftcrobocracy <ftcrobocracy@gmail.com>
2016-12-01 15:31:04 -0500

    Apply power to partcile accelerator gradually.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/attachments/ParticleAccelerator.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/attachments/ParticleAccelerator.java
@@ -4,6 +4,7 @@ import com.qualcomm.ftccommon.DbgLog;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.util.ElapsedTime;
 import com.qualcomm.robotcore.util.Range;
 
 import org.firstinspires.ftc.teamcode.FTCRobot;
@@ -19,6 +20,7 @@ public class ParticleAccelerator implements Attachment{
     FTCRobot robot;
     LinearOpMode curOpMode;
     DcMotor launcherMotor;
+    long rampUpTime = 2000; // default value in milli seconds
 
     public ParticleAccelerator(FTCRobot robot, LinearOpMode curOpMode, JSONObject rootObj) {
         String key;
@@ -39,8 +41,13 @@ public class ParticleAccelerator implements Attachment{
                 DbgLog.msg("Reversing the launcher motor");
                 launcherMotor.setDirection(DcMotorSimple.Direction.REVERSE);
             }
+            key = JsonReader.getRealKeyIgnoreCase(launcherMotorObj, "rampUpTime");
+            rampUpTime = launcherMotorObj.getLong(key);
             double maxSpeed = launcherMotorObj.getDouble("maxSpeed");
             launcherMotor.setMaxSpeed((int)(launcherMotor.getMaxSpeed() * maxSpeed));
+            // Set the zero power behaviour to float sot hat the motor stops gradually
+            // This is recommended for high speed low torque motors
+            launcherMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
         } catch (JSONException e) {
             e.printStackTrace();
         }
@@ -49,9 +56,16 @@ public class ParticleAccelerator implements Attachment{
     @Override
     public void getAndApplyDScmd() {
         if (curOpMode.gamepad1.dpad_up) {
-            launcherMotor.setPower(1.0);
+            // Ramp up the power gradually.
+            // This is recommended for high speed low torque motors
+            ElapsedTime rampUpTimer = new ElapsedTime(ElapsedTime.Resolution.MILLISECONDS);
+            rampUpTimer.reset();
+            while ((rampUpTimer.milliseconds() < rampUpTime) && curOpMode.opModeIsActive()) {
+                launcherMotor.setPower(1 - (rampUpTimer.milliseconds() / rampUpTime));
+            }
         }
         else if (curOpMode.gamepad1.dpad_down) {
+            // Zero power behaviour was set to FLOAT in the constructor.
             launcherMotor.setPower(0.0);
         }
     }
