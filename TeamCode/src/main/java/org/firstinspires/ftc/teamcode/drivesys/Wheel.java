ce20a4f0b4957361cdaf7424146c61ccdbbbe198
ftcrobocracy <ftcrobocracy@gmail.com>
2016-12-24 20:31:44 -0500

    After rebasing onto v2.4

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drivesys/Wheel.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drivesys/Wheel.java
@@ -1,33 +1,20 @@
 package org.firstinspires.ftc.teamcode.drivesys;
 
 public class Wheel {
-    public enum Type{MECANUM, OMNI, RUBBER_TREADED}
 
-    Type type;
-    public Wheel(Type type){
+    String type;
+    double diameter;
+    double circumference;
+
+    public Wheel(String type, double diameter)
+    {
         this.type = type;
+        this.diameter = diameter;
+        this.circumference = diameter * Math.PI;
     }
 
-    public double[] getValues(){
-        double[] values = new double[2];
-        switch (type){
-            case MECANUM:
-                values[0] = 4;
-                values[1] = 4 * Math.PI;
-                break;
-            case OMNI:
-                values[0] = 4;
-                values[1] = 4 * Math.PI;
-                break;
-            case RUBBER_TREADED:
-                values[0] = 4;
-                values[1] = 4 * Math.PI;
-                break;
-            default:
-                break;
-        }
-        return values;
-    }
+    public double getCircumference() { return (this.circumference); }
+    public double getRadius() { return (this.diameter); }
 
 
 }
