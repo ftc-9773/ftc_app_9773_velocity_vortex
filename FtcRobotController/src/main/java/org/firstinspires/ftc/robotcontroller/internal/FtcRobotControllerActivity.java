221058c6cbf508542f0710c63c155d0489e196e9
michaela10c <michaela10c@qq.com>
2016-12-30 15:49:13 -0500

    one todo added - make elseif r.ids into switch statements

--- FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/internal/FtcRobotControllerActivity.java
+++ FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/internal/FtcRobotControllerActivity.java
@@ -285,8 +285,7 @@ public class FtcRobotControllerActivity extends Activity {
     if (this.eventLoop != null) {
       for (;;) {
         UsbDevice usbDevice = receivedUsbAttachmentNotifications.poll();
-        if (usbDevice == null)
-          break;
+        if (usbDevice == null) break;
         this.eventLoop.onUsbDeviceAttached(usbDevice);
       }
     }
@@ -514,7 +513,7 @@ public class FtcRobotControllerActivity extends Activity {
   @Override
   public boolean onOptionsItemSelected(MenuItem item) {
     int id = item.getItemId();
-
+    //TODO: make the ids into switch statements
     if (id == R.id.action_programming_mode) {
       if (cfgFileMgr.getActiveConfig().isNoConfig()) {
         // Tell the user they must configure the robot before starting programming mode.
