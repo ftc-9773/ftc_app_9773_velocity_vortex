e56c249c10f2347dddc22acb9191f64b64be4a5f
michaela10c <michaela10c@qq.com>
2017-01-28 18:50:27 -0500

    New state machine added (shoot, lift, beacon)

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/StateMachine.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/StateMachine.java
@@ -2,29 +2,42 @@ package org.firstinspires.ftc.teamcode.util;
 
 //import android.support.annotation.TransitionRes;
 
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+
 /**
  * Created by michaelzhou on 12/30/16.
  */
 
 public class StateMachine {
     private enum State{
-        STRING_GAMESTATE1,
-        STRING_GAMESTATE2;
+       TeleOp_ShootParticles,
+        EndGame_CapBall,
+        TeleOp_BeaconClaim;
     }
 
     State state;
 
-    public void transition(String input){
-        switch(input){
-            case "gamePad1": setState(State.STRING_GAMESTATE1); break;
-            case "gamePad2": setState(State.STRING_GAMESTATE2); break;
-        }
+    public StateMachine(){
+        this.state = State.TeleOp_ShootParticles;
     }
 
-    void setState(State newState){state = newState;}
+    public void transition(OpMode opMode){
+        if(opMode.gamepad1.back){
+            switch (state){
+                case TeleOp_BeaconClaim: state = State.EndGame_CapBall;
+                    opMode.telemetry.addData("Current State: ","Cap Ball");
+                    break;
+                case EndGame_CapBall: state = State.TeleOp_ShootParticles;
+                    opMode.telemetry.addData("Current State: ","Shoot Particles");
+                    break;
+                case TeleOp_ShootParticles: state = State.TeleOp_BeaconClaim;
+                    opMode.telemetry.addData("Current State: ","Beacon Claim");
+                    break;
+            }
+        }
+
+    }
 
     public State getCurrentState(){return state;}
-
-
 
 }
